8fdeca643e3caa9499bbedeb6e01907218bb3796
shervheimb6 <shervheimb6@gmail.com>
2020-04-26 17:22:35 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+let temp = ['Body Temp a.m.', 'Body Temp p.m.','Pulse'];
+let headerNameList = [
+  'Fever',
+  'Cough',
+  'Running nose',
+  'Loss of Smell',
+  'Shortness of breath',
+  'Sore throat',
+  'Chills',
+  'Headache',
+  'Muscle pain',
+  'Fatigue',
+  'Cravings',
+  'Blood pressure',
+  'Blood oxygen level',
+  'Anything unusual',
+  '',
+  '',
+  '', 
+]
+
+$(document).ready(() => {
+  function createTableBody() {
+    // create table
+    $("<table></table>").appendTo("body");
+
+    // create header row
+    let tableHead = $("<thead></thead>").addClass('head').append("<tr></tr>");
+    tableHead.children().append("<th>Symptoms</th>");
+    for (let i = 1; i < 15; i++) tableHead.children().append(`<th>Day ${i}</th>`);
+    tableHead.appendTo("table");
+
+    // create table body
+    $("<tbody></tbody>").appendTo("table");
+
+    // create body temperature rows
+    for (let n in temp) {
+      let row = $("<tr></tr>");
+      $("<th></th>").text(temp[n]).appendTo(row);
+      for (let i = 0; i < 14; i++) {
+        let cell = $("<td></td>").appendTo(row).append('<input/>').attr('type','text');
+      };
+      $("tbody").append(row);
+      // adds row for pulse
+      $("tbody").append(row);
+    };
+    
+    // create checkbox rows
+    for (let n in headerNameList) {
+      let row = $("<tr></tr>").appendTo("tbody");
+      let header = $("<th></th>").text(headerNameList[n]).appendTo(row);
+      for (let i = 0; i < 14; i++) {
+        let cell = $("<td></td>").addClass('cell').appendTo(row);
+      };
+    };
+  }
+
+  // use function to create table
+  createTableBody();
+
+  // checkbox on/off
+  $('td').on('click', event => {
+    if ($(event.currentTarget).hasClass('cell')) {
+      $(event.currentTarget).html($(event.currentTarget).html() === 'X'? '': 'X').toggleClass('x');
+    };
+  });
+});
+
